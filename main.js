57027a066e392107d0396a1222c53339994329da
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 11:12:54 +0100

    träning om olika function

---
+++ main.js
@@ -0,0 +1,25 @@
+/* global console, alert */
+
+/*
+
+function = Function Keyword In JavaScript
+
+*/
+
+/*
+function sayHi () {
+
+    "use strict"; // för att ska inte vara Erur 
+    
+    alert("Hello JavaScript From Inside The Function");
+}
+*/
+
+function sayHi () {
+
+    "use strict";
+
+    var myName = "Abdallah";
+    
+    alert("Hello " + myName + " JavaScript From Inside The Function");
+}
