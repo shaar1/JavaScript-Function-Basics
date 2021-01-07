f7a90958564c4a8063927a553352af0a8bc39752
Abdallah <abdallahchaar90@gmail.comh>
2021-01-07 11:01:12 +0100

    träning om function

---
+++ style.js
@@ -0,0 +1,14 @@
+/* global console, alert */
+
+/*
+
+function = Function Keyword In JavaScript
+
+*/
+
+function sayHi () {
+
+    "use strict"
+    
+    alert("Hello JavaScript From Inside The Function")
+}
